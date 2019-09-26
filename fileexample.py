2f46955479e68262f02c454fbf712d41c41be208
Kalyan <gkalyan369@gmail.com>
2019-09-26 11:42:07 +0530

    added the assigment on files

---
+++ fileexample.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 11:28:34 2019
+
+@author: Abridge
+"""
+
+List = [["W2342323","kalyan","kalyan@chase.com"],["E24323423","Srini","srini@chase.com"],["y123123","vas","vas@chase.com"]]
+
+fileWritecontent = open ("Employee.txt",'w')
+for i in List :
+    fileWritecontent.write (str(i))
+fileWritecontent.close()
+
+with open ("Employee.txt","r") as file:
+    print(file.readlines())
